e0a5a3e0f677847f848710d79e6fb916cf1bcb5b
Flame <flame2001@inbox.ru>
2019-12-12 16:52:45 +0300

    commited

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp()); // COOMMENT
+void main() => runApp(MyApp()); // commit
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
